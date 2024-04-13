5e738aee0254615adbbcad95b23ec27b11523b94
alesan99 <alesanesp99@gmail.com>
2024-04-13 00:04:25 -0500

    Add CRATE

--- website/game.js
+++ website/game.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 
+import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
+
 export class GameClass {
 	constructor(renderer, scene, camera) {
 		this.renderer = renderer;
@@ -11,7 +13,8 @@ export class GameClass {
 		// Create a point light
 		const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
 		directionalLight.position.set(1, 1, 1); // default; light shining from top
-
+		const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+		scene.add( light );
 
 		this.scene.add(directionalLight); // Add the light to the scene
 		const material = new THREE.MeshLambertMaterial({
@@ -23,15 +26,39 @@ export class GameClass {
 		for (let i = 0; i < 10; i++) {
 			let b = new THREE.Mesh(geometry, material);
 			this.objects[i] = b;
-			b.position.y = Math.random() * 10 - 5;
+			b.position.y = i * 2 - 10;
 			this.scene.add( this.objects[i] );
 		}
 
 		this.scale = 1;
+
+		this.snake = null;
+
+		const loader = new GLTFLoader();
+
+		const game = this;
+		loader.load(
+			// resource URL
+			'assets/crate.glb',
+			// called when the resource is loaded
+			function ( gltf ) {
+				game.snake = gltf.scene;
+				game.scene.add( game.snake );
+			},
+			// called while loading is progressing
+			function ( xhr ) {
+				console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+			},
+			// called when loading has errors
+			function ( error ) {
+				console.log( 'An error happened', error );
+			}
+		);
 	}
 
 	update(dt) {
-		this.scale = (this.scale + 10*dt)%(Math.PI*2);
+		console.log(this.snake)
+		this.scale = (this.scale + 1*dt)%(Math.PI*2);
 		for (let i in this.objects) {
 			let b = this.objects[i];
 			b.position.x = Math.sin(this.scale + i)*2;
@@ -39,6 +66,7 @@ export class GameClass {
 			b.rotation.x = b.rotation.x + 1*dt;
 			b.scale.x = Math.sin(this.scale)*0.5+1;
 			b.scale.y = Math.sin(this.scale)*0.5+1;
+			b.scale.z = Math.sin(this.scale)*0.5+1;
 		}
 	}
 
---
+++ website/objects/object.js
@@ -0,0 +1,88 @@
+//Physics object base class
+
+export class PhysicsObject {
+	//Initialize: x pos, y pos
+	constructor (spatialHash, x, y) {
+		// Position and speed
+		this.x = x
+		this.y = y
+		this.sx = 0
+		this.sy = 0
+		this.static = false
+		this.active = true
+		
+		this.oldx = x
+		this.oldy = y
+
+		this.collisions = new Map()
+
+		// Spatial Hash location
+		this.spatialHash = spatialHash
+		this.cellx = 0
+		this.celly = 0
+		this.cellw = 0
+		this.cellh = 0
+	}
+
+	setPosition(x, y) {
+		if (x != null) {
+			this.x = x
+		}
+		if (y != null) {
+			this.y = y
+		}
+		this.updateCellLocation()
+	}
+
+	setShape(shape) {
+		this.shape = shape
+		this.updateCellLocation()
+	}
+
+	updateCellLocation() {
+		// New cell location? Check using bounding box of object's shape.
+		let spatialHash = this.spatialHash
+		let [cx, cy, cw, ch] = [this.cellx, this.celly, this.cellw, this.cellh]
+		let [ncx, ncy, ncw, nch] = spatialHash.getCoords(this.x+this.shape.x1,this.y+this.shape.y1,this.x+this.shape.x2,this.y+this.shape.y2)
+		if (!(cx == ncx && cy == ncy && cw == ncw && ch == nch)) {
+			// TODO: Optimize this
+			// Remove references to this object from old location
+			for (let x = cx; x <= cw; x++) {
+				for (let y = cy; y <= ch; y++) {
+					spatialHash.removeFromCell(x, y, this)
+				}
+			}
+			// Add references to this object in new location
+			for (let x = ncx; x <= ncw; x++) {
+				for (let y = ncy; y <= nch; y++) {
+					spatialHash.putToCell(x, y, this)
+				}
+			}
+			[this.cellx, this.celly, this.cellw, this.cellh] = [ncx, ncy, ncw, nch]
+		}
+	}
+
+	// Object collided; object name, object, normal x, normal y
+	collide(name, obj, nx, ny) {
+		return true
+	}
+
+	startCollide(name, obj) {
+		
+	}
+
+	stopCollide(name, obj) {
+		
+	}
+
+	destroy() {
+		let [cx, cy, cw, ch] = this.spatialHash.getCoords(this.x+this.shape.x1,this.y+this.shape.y1,this.x+this.shape.x2,this.y+this.shape.y2)
+		// Remove references to this object
+		for (let x = cx; x <= cw; x++) {
+			for (let y = cy; y <= ch; y++) {
+				this.spatialHash.removeFromCell(x, y, this)
+			}
+		}
+		this.DELETED = true
+	}
+}
